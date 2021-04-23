c99c0abaa70a6b5d058b632b6b762d06487b526f
Leonard Struck <leonardstruck@blockform.at>
2021-04-23 12:50:26 +0200

    boilerplate

---
+++ src/app/app.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+export const App = () => {
+	return <h1>Test</h1>;
+};
+
+ReactDOM.render(<App />, document.getElementById("app"));
---
+++ webpack.renderer.config.js
@@ -0,0 +1,21 @@
+const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
+const webpack = require("webpack");
+const rules = require("./webpack.rules");
+const isDevelopment = process.env.NODE_ENV === "development";
+
+rules.push({
+	test: /\.css$/,
+	use: [{ loader: "style-loader" }, { loader: "css-loader" }],
+});
+
+module.exports = {
+	// Put your normal webpack config below here
+	module: {
+		rules,
+	},
+	plugins: [
+		// ... other plugins
+		isDevelopment && new webpack.HotModuleReplacementPlugin(),
+		isDevelopment && new ReactRefreshWebpackPlugin(),
+	].filter(Boolean),
+};
